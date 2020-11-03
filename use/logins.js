a267add64c695efdaaeb52d3518d51fb1fc33437
Shawn Inder <shawninder@gmail.com>
2020-11-03 17:04:33 -0500

    remove one more log

--- use/logins.js
+++ use/logins.js
@@ -3,7 +3,6 @@ import jsonFetcher from '../lib/jsonFetcher'
 
 export default function useCount () {
   const { data: logins, isValidating, mutate } = useSWR('/api/logins', jsonFetcher())
-  console.log('logins', logins)
   return {
     logins,
     loading: isValidating,
