a287426ea1bba346e5539660a2d7f5c2edf0a1b5
Shawn Inder <shawninder@gmail.com>
2020-11-04 15:02:34 -0500

    🔥 Let's try debugging with `console.log` in production...

--- pages/api/logins.js
+++ pages/api/logins.js
@@ -2,20 +2,14 @@ import { createHandlers } from '../../lib/rest-utils'
 import { Login } from '../../models/login'
 
 const handlers = {
-  HEAD: (req, res) => {
-    res.status(200)
-  },
   GET: async (req, res) => {
     try {
       const loginModel = new Login()
       const logins = await loginModel.getAll()
-      console.log('===logins', logins)
-      console.error('===logins', logins)
       res.status(200).send(logins && logins.data)
     } catch (ex) {
-      console.log('ex', ex)
       console.error('ex', ex)
-      res.status(500).send({ ex: ex.ToString(), logins: [] })
+      res.status(500).send([])
     }
   }
 }
