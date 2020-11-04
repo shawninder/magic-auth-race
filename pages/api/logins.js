c46623610391fa66bb6ba5d30c9d4d9a26039f0e
Shawn Inder <shawninder@gmail.com>
2020-11-04 14:46:20 -0500

    🔥 Do I need to respond to HEAD requests?

--- pages/api/logins.js
+++ pages/api/logins.js
@@ -1,11 +1,22 @@
+import { createHandlers } from '../../lib/rest-utils'
 import { Login } from '../../models/login'
 
-export default async (req, res) => {
-  try {
-    const loginModel = new Login()
-    const logins = await loginModel.getAll()
-    res.status(200).send(logins && logins.data)
-  } catch (ex) {
-    res.status(500).send({ ex: ex.ToString(), logins: [] })
+const handlers = {
+  HEAD: (req, res) => {
+    res.status(200)
+  },
+  GET: async (req, res) => {
+    try {
+      const loginModel = new Login()
+      const logins = await loginModel.getAll()
+      res.status(200).send(logins && logins.data)
+    } catch (ex) {
+      res.status(500).send({ ex: ex.ToString(), logins: [] })
+    }
   }
+}
+
+export default function loginStats (req, res) {
+  const handler = createHandlers(handlers)
+  return handler(req, res)
 }
