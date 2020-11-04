2b4f2c03b9168c37797ce12b5d9fa26a920f1775
Shawn Inder <shawninder@gmail.com>
2020-11-03 23:27:40 -0500

    ❌ Don't revalidate on focus

--- use/logins.js
+++ use/logins.js
@@ -2,7 +2,9 @@ import useSWR from 'swr'
 import jsonFetcher from '../lib/jsonFetcher'
 
 export default function useLogins () {
-  const { data: logins, isValidating, mutate } = useSWR('/api/logins', jsonFetcher())
+  const { data: logins, isValidating, mutate } = useSWR('/api/logins', jsonFetcher(), {
+    revalidateOnFocus: false
+  })
   return {
     logins,
     loading: isValidating,
--- use/user.js
+++ use/user.js
@@ -2,7 +2,9 @@ import useSWR from 'swr'
 import jsonFetcher from '../lib/jsonFetcher'
 
 export default function useUser () {
-  const { data, isValidating } = useSWR('/api/user', jsonFetcher())
+  const { data, isValidating } = useSWR('/api/user', jsonFetcher(), {
+    revalidateOnFocus: false
+  })
   return {
     user: (data && data.user) || null,
     loading: isValidating
