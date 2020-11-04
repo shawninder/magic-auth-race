b633f61f0c8172031f44a06d5dde8f951d2a426a
Shawn Inder <shawninder@gmail.com>
2020-11-04 14:42:05 -0500

    🔥 Hotfix

--- pages/api/logins.js
+++ pages/api/logins.js
@@ -1,19 +1,11 @@
-import { createHandlers } from '../../lib/rest-utils'
 import { Login } from '../../models/login'
 
-const handlers = {
-  GET: async (req, res) => {
-    try {
-      const loginModel = new Login()
-      const logins = await loginModel.getAll()
-      res.status(200).send(logins && logins.data)
-    } catch (ex) {
-      res.status(500).send({ ex: ex.ToString(), logins: [] })
-    }
+export default async (req, res) => {
+  try {
+    const loginModel = new Login()
+    const logins = await loginModel.getAll()
+    res.status(200).send(logins && logins.data)
+  } catch (ex) {
+    res.status(500).send({ ex: ex.ToString(), logins: [] })
   }
-}
-
-export default function loginStats (req, res) {
-  const handler = createHandlers(handlers)
-  return handler(req, res)
 }
