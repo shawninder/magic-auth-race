af903a1d7c14fef30574e292a1370d114226f547
Shawn Inder <shawninder@gmail.com>
2020-11-04 14:50:21 -0500

    🔥 Let's try debugging with `console.log` in production...

--- pages/api/logins.js
+++ pages/api/logins.js
@@ -9,8 +9,12 @@ const handlers = {
     try {
       const loginModel = new Login()
       const logins = await loginModel.getAll()
+      console.log('===logins', logins)
+      console.error('===logins', logins)
       res.status(200).send(logins && logins.data)
     } catch (ex) {
+      console.log('ex', ex)
+      console.error('ex', ex)
       res.status(500).send({ ex: ex.ToString(), logins: [] })
     }
   }
